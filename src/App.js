3a049e55c4c70cf27753e64d776e8c2b1ae796b6
Diogo D. Reis <diogo.paulino10@gmail.com>
2025-03-20 19:06:26 +0000

    deploy website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import PortfolioPage from "./pages/PortfolioPage";
 import About from "./pages/About";
