df54afcc2390d596d8f165db06477ceed2e31405
Diogo D. Reis <diogo.paulino10@gmail.com>
2025-04-08 22:40:21 +0100

    fixs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import PortfolioPage from "./pages/PortfolioPage";
 import About from "./pages/About";
